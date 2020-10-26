8d319f8e8fe788997e31264fff51e9cb91eea0c6
eun <lisa1402@naver.com>
2020-10-26 22:33:37 +0900

    10월 26일

---
+++ blog/views.py
@@ -0,0 +1,16 @@
+from django.shortcuts import render
+from .models import Blog
+# Create your views here.
+
+def index(request):
+    return render(request, 'index.html')
+
+
+
+
+
+
+    
+
+    
+
